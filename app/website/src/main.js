fd8e0c92afaec481df55f05e208ba8f4e6516cdf
liangsqrt <liangsqrt@163.com>
2026-03-20 12:06:42 +0800

    feat: 样式回归绿色

--- app/website/src/main.js
+++ app/website/src/main.js
@@ -3,7 +3,6 @@ import * as bootstrap from 'bootstrap';
 import Chart from 'chart.js/auto';
 import App from './App.vue';
 import { loadLegacyScripts } from './legacy/loadLegacyScripts';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/styles.css';
 
 window.bootstrap = bootstrap;
